5d82b62c052eabe514ca2eab64802ac58e6ca89b
6579321 <yasmeen.shaik99@gmail.com>
2024-12-21 11:09:56 -0500

    dog

---
+++ dog.py
@@ -0,0 +1,11 @@
+class dog:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+GermanShepphard = dog('German Shepphard','Brown')
+Poodle = dog('Poodle', 'White')
+
+print(GermanShepphard.name, GermanShepphard.color)
+print(Poodle.name, Poodle.color)
+
